0ddabcc9e3e5f9c7b4d2a53ad9c87def08ac7d2a
Muhammad Abdullah <67463724+Muhammad-Abdullah-Rana@users.noreply.github.com>
2022-09-28 12:12:55 +0500

    Add project files.

---
+++ enc_temp_folder/b44c62a15f75ef743f2265d7df5bd144/White To Red Video Pixel Conversion.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    string path = "videos/test_video.mp4";
+    VideoCapture cap(path);
+    Mat currFrame, currWhitePixels;
+
+    cap.read(currFrame);
+
+    while (!currFrame.empty()) {
+        inRange(currFrame, Scalar(255, 255, 255), Scalar(255, 255, 255), currWhitePixels);
+        currFrame.setTo(Scalar(0, 0, 255), currWhitePixels);
+        imshow("Video", currFrame);
+        waitKey(10);
+        cap.read(currFrame);
+    }
+    return 0;
+}
